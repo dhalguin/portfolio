f3181cb72c426c68f5e18f04f9a30ef863415539
Dhalguin Hernandez <dhalguinjesus21nov@gmail.com>
2026-03-03 22:50:19 -0400

    update work experience

--- src/constants/experience.tsx
+++ src/constants/experience.tsx
@@ -16,10 +16,38 @@ export const workExperience = (t: (key: TranslationKey) => string | string[]) =>
   const data: WorkExperienceType[] = [
     {
       id: 0,
+      role: t('experience_vero_title') as string,
+      company: 'Vero',
+      startDate: 'Nov 2025',
+      endDate: 'Presente',
+      keyPoints: [
+        t('experience_vero_description')[0],
+        t('experience_vero_description')[1],
+        t('experience_vero_description')[2],
+        t('experience_vero_description')[3],
+      ],
+      imageUrl: '',
+    },
+    {
+      id: 1,
+      role: t('experience_booknow_title') as string,
+      company: 'Booknow',
+      startDate: 'Sep 2025',
+      endDate: 'Jan 2026',
+      keyPoints: [
+        t('experience_booknow_description')[0],
+        t('experience_booknow_description')[1],
+        t('experience_booknow_description')[2],
+        t('experience_booknow_description')[3],
+      ],
+      imageUrl: '',
+    },
+    {
+      id: 2,
       role: t('experience_rentaweb_title') as string,
-      company: 'Rentaweb.cl',
-      startDate: 'Mar 2023',
-      endDate: 'Jan 2025',
+      company: 'Rentaweb',
+      startDate: 'May 2023',
+      endDate: 'Jul 2025',
       keyPoints: [
         t('experience_rentaweb_description')[0],
         t('experience_rentaweb_description')[1],
@@ -30,7 +58,7 @@ export const workExperience = (t: (key: TranslationKey) => string | string[]) =>
       imageUrl: '',
     },
     {
-      id: 1,
+      id: 3,
       role: t('experience_drimo_title') as string,
       company: 'Drimo',
       startDate: 'Apr 2022',
@@ -43,7 +71,7 @@ export const workExperience = (t: (key: TranslationKey) => string | string[]) =>
       imageUrl: '',
     },
     {
-      id: 2,
+      id: 4,
       role: t('experience_freelance_title') as string,
       company: 'Freelance',
       startDate: 'Sep 2021',
